460b2976b3b5f7318e45c6e29b3bf42ee2647cb4
matthewenger <m.enger@xi.com.au>
2016-09-21 22:30:11 +1000

    Infotrans integration - file generation

--- table_changes.sql
+++ table_changes.sql
@@ -7,3 +7,29 @@ alter table wholesalers add xero_name varchar(255) not null;
 alter table wholesalers add xero_apikey varchar(255) not null;
 
 alter table customers add xero_contactid varchar(255) not null;
+
+CREATE TABLE telstra_infotrans_files (
+file_seq int unsigned not null,
+creation date not null,
+primary key (file_seq)
+);
+
+CREATE TABLE telstra_infotrans_records (
+id bigint unsigned not null auto_increment,
+seq bigint unsigned,
+file_seq int unsigned not null,
+type tinyint unsigned not null,
+order_id int unsigned not null,
+param1 varchar(100) not null,
+param2 varchar(100) not null,
+param3 varchar(100) not null,
+param4 varchar(100) not null,
+param5 varchar(100) not null,
+param6 varchar(100) not null,
+primary key (id),
+unique (seq),
+ts timestamp,
+key (file_seq)
+);
+
+
